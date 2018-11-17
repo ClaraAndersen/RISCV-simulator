37a2434c7690074250e89323aeeef14bd49285e3
Atli Olsen <atliolsen91@gmail.com>
2018-11-17 16:53:57 +0100

    Array with hex

--- RiscV-simulator/src/BinaryFileToHex.java
+++ RiscV-simulator/src/BinaryFileToHex.java
@@ -5,13 +5,21 @@ import java.nio.file.Paths;
 
 public class BinaryFileToHex {
 	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-	public static void main(String[] args) throws IOException {
+//	public static void main(String[] args) throws IOException {
+//		Path path = Paths.get("./src/addlarge.bin");
+//		byte[] data = Files.readAllBytes(path);
+//		System.out.println(bytesToHex(data));
+	
+	
+	// Reads the binary file containing the instruction set into a byte array.
+	public byte[] readByteArray() throws IOException{
 		Path path = Paths.get("./src/addlarge.bin");
 		byte[] data = Files.readAllBytes(path);
-		System.out.println(bytesToHex(data));
+		return data;
 	}
-
-	public static String bytesToHex(byte[] bytes) {
+	
+	// Converts the byte array into a Hex - string.
+	public String bytesToHex(byte[] bytes) {
 		char[] hexChars = new char[bytes.length * 2];
 		for ( int j = 0; j < bytes.length; j++ ) {
 			int v = bytes[j] & 0xFF;
@@ -20,6 +28,28 @@ public class BinaryFileToHex {
 		}
 		return new String(hexChars);
 	}	
+	
+	public String ConvertedByteToHex() throws IOException {
+		return bytesToHex(readByteArray());	
+	}
+	
+	public String[] hexToArray() throws IOException{
+
+		BinaryFileToHex binaryFile = new  BinaryFileToHex();
+		String s = binaryFile.ConvertedByteToHex(); 
+
+		StringBuilder str = new StringBuilder(s);
+		int idx = str.length() - 8;
+
+		while (idx > 0){
+			str.insert(idx, ",");
+			idx = idx - 8;
+		}
+		String[] array = new String[]{str.toString()};
+		return array;
+	}
 }
+
+
 
 
