79127a31258abd959856a000b80203e0e2892bf2
Atli Olsen <atliolsen91@gmail.com>
2018-11-19 14:36:48 +0100

    BinaryFile to Hexadecimal appears to be fully working. Ready for implementation in the simulator.

--- RiscV-simulator/src/BinaryFileToHex.java
+++ RiscV-simulator/src/BinaryFileToHex.java
@@ -1,47 +1,23 @@
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Array;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.regex.Pattern;
 
 
 public class BinaryFileToHex {
 	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-	private String[] words;
-	private int i;
-	//	public static void main(String[] args) throws IOException {
-	//		Path path = Paths.get("./src/addlarge.bin");
-	//		byte[] data = Files.readAllBytes(path);
-	//		System.out.println(bytesToHex(data));
 
-
-	// Reads the binary file containing the instruction set into a byte array.
-	// Maybe not fully useful.
+	// Method that reads the binary file containing the instruction 
+	// set into a byte array.
 	public byte[] readByteArray() throws IOException{
 		Path path = Paths.get("./src/test_addi.bin");
 		byte[] data = Files.readAllBytes(path);
 		return data;
-//		List<byte[]> listOfByte = Arrays.asList(data);	
-//		Collections.reverse(listOfByte);
-//		
-//		byte[] result = listOfByte.stream().collect(
-//				() -> new ByteArrayOutputStream(),
-//				(b, e) -> {
-//					try {
-//						b.write(e);
-//					} catch (IOException e1) {
-//						throw new RuntimeException(e1);
-//					}
-//				},
-//				(a, b) -> {}).toByteArray();
-//		return result;
 	}
-	// Reverse
+	// Method that reverses the order of the byte array.
+	// Because we want to prepare for converting from little edian to big edian 
 	public byte[] reverse(byte[] array) {
 	      int i = 0;
 	      int j = array.length - 1;
@@ -56,20 +32,18 @@ public class BinaryFileToHex {
 		return array;
 	  }
 
-	// Converts the byte array into a Hex - string.
+	// Method that converts the byte array into a Hex - string.
 	public String bytesToHex(byte[] bytes) {
-		
 		char[] hexChars = new char[bytes.length * 2];
 		for ( int j = 0; j < bytes.length; j++ ) {
 			int v = bytes[j] & 0xFF;
 			hexChars[j * 2] = hexArray[v >>> 4];
 			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
 		}
-//		String newString = new StringBuilder(new String(hexChars)).reverse().toString();
 		return new String(hexChars);
 	}	
 
-	// Returns the Hex with 0x in the start.
+	// Method that returns the Hex with 0x in the start.
 	public String ConvertedByteToHex() throws IOException {
 		String myString = bytesToHex(reverse(readByteArray()));
 //		String newString = "0x" + myString.replaceAll("(.{8})(?!$)", "$1 0x");
@@ -77,23 +51,24 @@ public class BinaryFileToHex {
 		return newString;
 	}
 
-	// Return hex in a String array where we can access the indices with " HexArray()[1] "
+	// Return hex in a String array where we can access the indices with " HexArray()[1]"
+	// and reverse the order of the elements in the string array.
 	public String[] HexArray() throws IOException {
 		String line = ConvertedByteToHex();
 		Pattern pattern = Pattern.compile(" ");
-		words = pattern.split(line);
-		return words;
-	
-	}
-	
-	public String[] reverseString(String[] words) {
-		String[] t = new String[words.length];
-		for (String wordTemp : words) {
-			StringBuilder sb = new StringBuilder(wordTemp);
-			t[i] = sb.reverse().toString();   
+		String [] words = pattern.split(line);
+		Arrays.toString(words);
+		for(int i = 0; i < words.length/2; i++) {
+			String temp = words[i];
+			words[i] = words[words.length - i - 1];
+			words[words.length - i - 1] = temp;
 		}
-		return t;
+		return words;
 	}
+	
+	
+	
+
 }
 
 
--- RiscV-simulator/src/Main.java
+++ RiscV-simulator/src/Main.java
@@ -1,59 +1,18 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 
 public class Main extends BinaryFileToHex{
 
 	public static void main(String[] args) throws IOException {
 
+		// Testing 
 		BinaryFileToHex binaryFile = new  BinaryFileToHex();
-//		System.out.println(Arrays.toString(binaryFile.HexArray()));
-		
-//		System.out.println(binaryFile.ConvertedByteToHex());
-//		
-		String test = binaryFile.HexArray()[1];
-		System.out.println("Hex: "+test);
-//		
+		System.out.println("Original Array : "+Arrays.toString(binaryFile.HexArray()));
+		System.out.println("Flipped bytes  : "+binaryFile.ConvertedByteToHex());
+		String test = binaryFile.HexArray()[4];
+		System.out.println("Hex as index   : "+test);
+		//		
 
-//		int hello = Integer.parseInt(test,16);
-//		System.out.println("Decimal of hex: "+hello);
-		
-//		short s = (short) Integer.parseInt(test,16);
-//		System.out.println(s);
-//		System.out.println(unsignedByteToInt(hello));
-		
-//		String hex = Integer.toHexString(s);
-//		System.out.println(hex);
-	
-//		int something = Integer.parseUnsignedInt(test);
-//		System.out.println(something);
-		
-//		int intVal = Integer.parseInt(test, 16);
-//		String twosComplement = Integer.toHexString((-1 * intVal));
-//		System.out.println(twosComplement);
-		
-//		String hex = test;
-//
-//		// First convert the Hex-number into a binary number:
-//		String bin = Long.toString(Integer.parseInt(hex, 16), 2);
-		
-
-		// Now create the complement (make 1's to 0's and vice versa)
-//		String binCompl = bin.replace('0', 'X').replace('1', '0').replace('X', '1');
-//
-//		// Now parse it back to an integer, add 1 and make it negative:
-//		int result = (Integer.parseInt(binCompl, 2) + 1) * -1;
-//		
-//		System.out.println("Convert    : "+result);
-//		System.out.println("Binary     : "+bin);
-//		System.out.println("BinaryCompl: "+binCompl);
-		
-//		
-//		System.out.println(Arrays.toString(binaryFile.readByteArray()));
-//		Arrays.copyOfRange(binaryFile.readByteArray(),  6,  binaryFile.readByteArray().length);
-		
 	}
-	
-//	  public static int unsignedByteToInt(int s) {
-//		    return (int) s & 0xFF;
-//		  }
 }
